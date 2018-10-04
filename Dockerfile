fa59be394dae7abbaf561895788f71fd1e61dcd2
Oleg Atamanenko <oatamanenko@eastbanctech.com>
2018-10-04 18:01:49 -0400

    Added prometheus exporter

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM golang:1.11.1
 
+# RUN go get -u github.com/golang/dep/cmd/dep
+
 ADD . /go/src/spinnaker.io/demo/k8s-demo
+
+# WORKDIR /go/src/spinnaker.io/demo/k8s-demo
+# RUN dep ensure
 
 RUN go install spinnaker.io/demo/k8s-demo
 
--- main.go
+++ main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
-	"os"
-	"fmt"
+	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Handling %+v\n", r);
-	bs, err := ioutil.ReadFile("/content/index.html")
+	if rand.Int63n(100) < errRate {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	// fmt.Printf("Handling %+v\n", r)
+	bs, err := ioutil.ReadFile("./content/index.html")
 
 	if err != nil {
-		fmt.Printf("Couldn't read index.html: %v", err);
+		fmt.Printf("Couldn't read index.html: %v", err)
 		os.Exit(1)
 	}
 
 	io.WriteString(w, string(bs[:]))
 }
 
+var errRate int64 = 0
+
 func main() {
-	http.HandleFunc("/", index)
+	failRate := os.Getenv("FAIL_RATE")
+	if failRate != "" {
+		var err error
+
+		errRate, err = strconv.ParseInt(failRate, 10, 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	http.HandleFunc("/", prometheus.InstrumentHandlerFunc("index", index))
+	http.Handle("/metrics", promhttp.Handler())
 	port := ":8000"
-	fmt.Printf("Starting to service on port %s\n", port);
+	fmt.Printf("Starting to service on port %s\n", port)
 	http.ListenAndServe(port, nil)
 }
